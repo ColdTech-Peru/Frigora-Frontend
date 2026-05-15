1d6b028b35512cdf4e3f01ba5b264775ed628371
Alejandro <u202321264@upc.edu.pe>
2026-05-15 01:46:38 -0500

    fix: update monitoring-routes.ts
    Removed the route for the EquipmentFormComponent.

--- src/app/monitoring/presentation/monitoring-routes.ts
+++ src/app/monitoring/presentation/monitoring-routes.ts
@@ -3,16 +3,11 @@ import { Routes } from '@angular/router';
 import { EquipmentListComponent } from './views/equipment-list/equipment-list.component';
 import { EquipmentDetailComponent } from './views/equipment-detail/equipment-detail.component';
 import { AlertsListComponent } from './views/alerts-list/alerts-list.component';
-import { EquipmentFormComponent } from './views/equipment-form/equipment-form';
 
 export const monitoringRoutes: Routes = [
   {
     path: 'equipments',
     component: EquipmentListComponent
-  },
-  {
-    path: 'equipments/new',
-    component: EquipmentFormComponent
   },
   {
     path: 'equipments/:id',
