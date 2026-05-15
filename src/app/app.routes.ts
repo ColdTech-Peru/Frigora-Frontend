166a1e281b5b351ba37c3c08da3137a8db1bad7d
Alejandro <u202321264@upc.edu.pe>
2026-05-15 01:02:55 -0500

    fix: update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,11 @@
 import { Routes } from '@angular/router';
-import { Layout } from './shared/presentation/components/layout/layout';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: Layout,
-    children: [
-      {
-        path: 'reporting',
-        loadChildren: () =>
-          import('./reporting/reporting.routes')
-            .then(m => m.REPORTING_ROUTES)
-      }
-    ]
+    path: 'reporting',
+    loadChildren: () =>
+      import('./reporting/reporting.routes')
+        .then(m => m.REPORTING_ROUTES)
   },
 
   {
