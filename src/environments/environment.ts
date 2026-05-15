76b456aa98337caa62659d95b5f5331c7f1a8795
Alejandro <u202321264@upc.edu.pe>
2026-05-15 04:14:03 -0500

    feat: add fake-api deploy for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   // Base URL
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://fake-api-0233.onrender.com',
 
   tenantsEndpointPath: '/tenants',
   dashboardConfigsEndpointPath: '/dashboard-configs',
