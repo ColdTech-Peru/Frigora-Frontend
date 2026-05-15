0ab2e887f7657214f7eddd46ec8f678b83e15b36
JoaCUPE <u20201f788@upc.edu.pe>
2026-05-15 01:03:50 -0500

    feat: fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,9 @@ import { Layout } from './shared/presentation/components/layout/layout';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: Layout,
-    children: [
-
-      // AQUÍ AGREGAMOS LA RUTA PRINCIPAL DE SITIOS
-      {
-        path: 'sites',
-        loadChildren: () =>
-          import('./assets-management/presentation/assets-management-routes')
-            .then(m => m.assetsManagementRoutes)
-      }
-    ]
+    path: 'sites',
+    loadChildren: () =>
+      import('./assets-management/presentation/assets-management-routes')
+        .then(m => m.assetsManagementRoutes)
   }
 ];
